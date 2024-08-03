589a4d4a5c493f834dcf600d544e7d10b9bc0d9e
vtpqui <vtpqui@gmail.com>
2024-08-03 21:37:04 +0700

    angular starter project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MEALS_DUMMY_DATA } from './meals-dummy-data';
-import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CurrencyPipe],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  isOpenModal = false;
-  dummyData = MEALS_DUMMY_DATA;
-
-  open() {
-    this.isOpenModal = true;
-    this.addBodyClass();
-  }
-  cancel() {
-    this.isOpenModal = false;
-    this.addBodyClass();
-  }
-
-  addBodyClass() {
-    if (this.isOpenModal) {
-      document.body.classList.add('modal-open');
-    } else {
-      document.body.classList.remove('modal-open');
-    }
-  }
-}
+export class AppComponent {}
